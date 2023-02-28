c90fbcb2ef47e7b618149810bf03a80ca1ca439c
Guillermo Pulido <guillermopul@gmail.com>
2023-02-28 23:06:32 +0100

    Improvement in DOM control by html data attributes and major css update

--- Projects/1-Beginner/Border-Radius-Previewer/script.js
+++ Projects/1-Beginner/Border-Radius-Previewer/script.js
@@ -1,53 +1,26 @@
-let results_elem = document.getElementById('results_elem');
-let all_inputs = document.querySelectorAll(".input_container > div");
+var shape = document.getElementById('shape');
+let inputs = document.querySelectorAll(".input_container > div");
 
 
-let top_left = document.querySelector("[data-input='top-left']");
-let top_right = document.querySelector("[data-input='top-right']");
+//Set initial inputs random value
+inputs.forEach(input => {
+    let rand_percent = Math.floor(Math.random() * (100 - 15)) + 15;
 
-let right_top = document.querySelector("[data-input='right-top']");
-let right_bottom = document.querySelector("[data-input='right-bottom']");
+    input.style[input.getAttribute("data-side")] = rand_percent +"%";
+    input.setAttribute("data-value", rand_percent);
+});
 
-let bottom_left = document.querySelector("[data-input='bottom-left']");
-let bottom_right = document.querySelector("[data-input='bottom-right']");
-
-let left_top = document.querySelector("[data-input='left-top']");
-let left_bottom = document.querySelector("[data-input='left-bottom']");
+//Set the initial shape
+update_shape();
 
 
-//Top
-top_left.style.left = Math.floor(Math.random() * 50) +"%";
-top_right.style.right = Math.floor(Math.random() * 50) +"%";
 
-//Right
-right_top.style.top = Math.floor(Math.random() * 50) +"%";
-right_bottom.style.bottom = Math.floor(Math.random() * 50) +"%";
-
-//Bottom
-bottom_left.style.left = Math.floor(Math.random() * 50) +"%";
-bottom_right.style.right = Math.floor(Math.random() * 50) +"%";
-
-//Left
-left_top.style.top = Math.floor(Math.random() * 50) +"%";
-left_bottom.style.bottom = Math.floor(Math.random() * 50) +"%";
-
-//Initial border radius status
-results_elem.style.borderRadius = 
-    top_left.style.left +
-    top_right.style.right +
-    bottom_right.style.right +
-    bottom_left.style.left +
-    " / " +
-    left_top.style.top +
-    right_top.style.top +
-    right_bottom.style.bottom +
-    left_bottom.style.bottom 
-;
 
 //MIRAAAAAR https://www.kirupa.com/html5/drag.htm
 
 
-all_inputs.forEach(item => {
+inputs.forEach(item => {
+
     item.addEventListener("mousedown", mouseDown);
     item.addEventListener("mouseup", stopDrag);
     item.addEventListener("mouseout", stopDrag);
@@ -59,8 +32,9 @@ function mouseDown(e) {
     e = e || window.event;
     e.preventDefault();
 
-    initialY = e.clientY;
-    console.log(initialY);
+    initialX = e.clientX;
+    console.log(e.target.parentElement.offsetWidth, e.target.offsetLeft);
+    //console.log(e.target.parentElement.offsetHeight, e.target.offsetTop);
 
     // call a function whenever the cursor moves:
     e.target.addEventListener("mousemove", elementDrag);
@@ -69,21 +43,54 @@ function mouseDown(e) {
 function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-
-    // calculate the new cursor position:
-    let newY = initialY - e.clientY;
    
     //let posY = e.clientY;
 
-    let elemPosY = parseInt(e.target.style.top.replace('%',''));
-    e.target.style.top = elemPosY -  newY + '%';
-    console.log((elemPosY +  newY), initialY, e.clientY, newY);
 
+    let elem_val = parseInt(e.target.getAttribute("data-value"));
+    elem_val += initialX -  e.clientX;
+
+    e.target.setAttribute("data-value", elem_val);
+
+    e.target.style[e.target.getAttribute("data-side")] = elem_val +"%";
+
+    console.log(initialX, e.clientX, e.clientX);
+
+
+    //Update shape
+    update_shape();
     // set the element's new position:
     //e.target.style.top = (elmnt.offsetTop - pos2) + "px";
 }
 
+
 function stopDrag(e) {
     console.log("salimos");
     e.target.removeEventListener("mousemove", elementDrag);
+}
+
+
+
+//Set the shape border radius values
+function update_shape(){
+    
+    shape.style.borderTopLeftRadius = 
+        document.querySelector("[data-slider='top'] > [data-side='left']").getAttribute("data-value").concat("% ") +
+        document.querySelector("[data-slider='left'] > [data-side='top']").getAttribute("data-value").concat("%")
+    ;
+
+    shape.style.borderTopRightRadius = 
+        document.querySelector("[data-slider='top'] > [data-side='right']").getAttribute("data-value").concat("% ") +
+        document.querySelector("[data-slider='right'] > [data-side='top']").getAttribute("data-value").concat("%")
+    ;
+    
+    shape.style.borderBottomLeftRadius = 
+        document.querySelector("[data-slider='bottom'] > [data-side='left']").getAttribute("data-value").concat("% ") +
+        document.querySelector("[data-slider='left'] > [data-side='bottom']").getAttribute("data-value").concat("%")
+    ;
+
+    shape.style.borderBottomRightRadius = 
+        document.querySelector("[data-slider='bottom'] > [data-side='right']").getAttribute("data-value").concat("% ") +
+        document.querySelector("[data-slider='right'] > [data-side='bottom']").getAttribute("data-value").concat("%")
+    ;
 }
